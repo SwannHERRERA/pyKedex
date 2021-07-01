e77ecbdd2870c7c935cf7ee205472df5f91a06da
HERRERA Swann <swann@devloup.Dev>
2021-07-01 15:59:35 +0200

    use typer

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
 import requests
 from config import Config
+import typer
+
+app = typer.Typer()
 config = Config('https://pokeapi.co/api/v2')
 
-def getPokemons():
+@app.command()
+def get_pokemons():
   res = requests.get(config.baseUrl + '/pokemon')
   pokes = res.json()
   print(pokes['count'])
   print(pokes['results'][5]['name'])
 
-getPokemons()
+@app.command()
+def get_pokemon():
+  id = 4
+  res = requests.get(config.baseUrl + f'/pokemon/{id}')
+  pokes = res.json()
+  print(pokes['name'])
+  print(pokes['types'][0]['type']['name'])
+  print(pokes['weight'])
+  print("{}: {}".format(pokes['stats'][0]['stat']['name'], pokes['stats'][0]['base_stat']))
+
+if __name__ == "__main__":
+    app()
