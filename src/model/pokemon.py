4f45f325d6cef5460ce878417e269c58dc737a51
Swann <swann@devloup.dev>
2021-07-08 21:38:07 +0200

    add true pokemon

--- src/model/pokemon.py
+++ src/model/pokemon.py
@@ -1,3 +1,9 @@
 class Pokemon:
-    def __init__(self, name):
+    def __init__(self, name, speed, attack, life, imgUrl, types, moves):
         self.name = name
+        self.speed = speed
+        self.attack = attack
+        self.life = life
+        self.imgUrl = imgUrl
+        self.types = types
+        self.moves = moves
